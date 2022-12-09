6ad64876d8a972a1f2dd217a685109c3602f7726
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-09 16:44:53 +0000

    Express tested and confirmed working for us

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const app = express ()
 
+app.get("/", (req, res) => { // '/' is the web page or index page we will end up on when we type in localhost:5000 into the browser
+  res.send("Hi I am the response") // Conventionally we will use objects, however for now we can input a string to test if it works or not
+})
+
 app.listen(5000, () => {
   console.log("Server Started On Port 5000")
 })
