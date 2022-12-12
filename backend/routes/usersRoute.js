018d9a762865e13d0fb330e0a507169e095da299
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-12 18:22:48 +0000

    Hard coded a user to be saved into database - passed

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const User = require("../models/User")
 const router = express.Router()
 
 router.post("/register", (req, res) => { 
+
+  const me = new User({email: "a@a.com", password: "123456"})
+  console.log(me)
   res.status(200).send()
 })
 
