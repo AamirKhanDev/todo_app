990ce2dc6cf168160a3b00774c706967f7f7b923
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-13 02:25:31 +0000

    Bcrypt encrypt added on

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const User = require("../models/User")
 const router = express.Router()
 const {isEmail} = require("validator")
+const {genSaltSync, hashSync} = require("bcryptjs")
 
 router.post("/register", async (req, res) => { 
     try {
@@ -9,13 +10,14 @@ router.post("/register", async (req, res) => {
       const count = await User.count({email: req.body.email})
       if (count) throw new Error ("Email is not unique!")
       const user = new User(req.body)
+      //encrypt password then save
+      user.password = hashSync(user.password, genSaltSync(+process.env.SALT_ROUNDS));
+      //saves user registered
       await user.save()
       res.status(201).send(user)
   }   catch ({message}) {  res.status(400).send({message})
   }
- 
 
-  res.status(200).send()
 })
 
 router.post("/login", (req, res) => { 
