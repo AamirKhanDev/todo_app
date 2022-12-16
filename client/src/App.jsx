0334a78e962fc40651e1e0db1fb4e538e0c0c32a
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-16 05:19:38 +0000

    Imported routes into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 export default function App () {
     return (
-        <div className="wrapper bg-dark text-white">
-            <div className="container pt-5">
-                <h1 className="text-center">Hello React World</h1>
-            </div>
+      <div className='wrapper bg-dark text-white'>
+        <div className='container'>
+            <Routes>
+                <Route path = "/" element = {<Home/>} />
+                <Route path = "/login" element = {<Home/>} />
+                <Route path = "/register" element = {<Home/>} />
+            </Routes>
+
         </div>
+     </div>
     ) 
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.json"
 import "./main.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+root.render(
+  <Router>
+    <App/>
+</Router>
+)
