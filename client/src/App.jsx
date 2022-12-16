a7a55f94ae815c960a11f9a24641562a702f82c0
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-16 05:31:15 +0000

    Imported Header and Footer placeholders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export default function App () {
     return (
       <div className='wrapper bg-dark text-white'>
+            <Navbar />
         <div className='container'>
             <Routes>
                 <Route path = "/" element = {<Home/>} />
@@ -15,6 +18,7 @@ export default function App () {
             </Routes>
 
         </div>
+            <Footer />
      </div>
     ) 
 }
