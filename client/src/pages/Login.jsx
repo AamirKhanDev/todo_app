90bd0f6c49519857f436485a2ba3730c150a413d
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-18 05:39:51 +0000

    Add useState and placeholder form change/submit notification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,32 @@
-
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 
 export default function Login () {
+
+  const nav = useNavigate()
+
+  const [form, setForm] = useState ({email: "a@a.com", password: "123" })
+
+  const onFormChange = (e) => {
+    console.log("Form was changed")
+  }
+
+  const submit = () => {
+    alert ("Submitted")
+  }
+
   return (
    <div className="row text-white pt-5">
     <div className="col-12 col-lg-6 mx-auto">
-      <form>
+      <form onSubmit={submit} className="mx-auto">
         <h2 className="mb-4 text-center">Login</h2>
         <div className="row gy-4">
           <div className="col-12">
             <label className="form-label">Email</label>
             <input
-              onChange={() => {}}
-              value={""}
+              onChange={onFormChange} 
+              value={form.email}
               name="email"
               type="email"
               className="form-control"
@@ -31,8 +45,12 @@ export default function Login () {
             /> 
           </div>
         </div>
-        <span className="text-success my-3 h6 d-block text-center pointer">Register Here</span>
-        <button type="submit" className="w-100 btn btn-success mt-3"> Login</button>         
+        <span 
+          onClick={()=> nav("/register")}
+          className="text-success my-3 h6 d-block text-center pointer">Register Here
+          </span>
+        <button type="submit" className="w-100 btn btn-success mt-3"> Login
+        </button>         
       </form>
     </div>
    </div>
