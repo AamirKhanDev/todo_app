19ec87a82a1f00975c24450c2ad2f97f8bf442a6
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-19 21:50:10 +0000

    Update to list component. Added in map + key

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,25 +1,34 @@
 import Icon from "./Icon"
 import TodoText from "./ToDoText"
+import { useSelector } from "react-redux"
 
 export default function List () {
+  const { isLoading, todos } = useSelector (state=>state.todos)
+
+
   return (
     <ul id="list" className="list-group rounded-0 pb-3">
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <TodoText/>
-        <div>
-          <Icon
-            onClick={() =>{}}
-            type="pencil-square"
-            color="warning"
-            class={("me-3")}
-          />
-          <Icon
-            onClick={() =>{}}
-            type="trash"
-            color="danger"
-          />
-        </div>
-      </li>
+        {todos.map((todo, index) => (
+            <li 
+                key={todo._id} 
+                className="list-group-item d-flex justify-content-between align-items-center"
+            >
+                <TodoText todo={todo}/>
+                <div>
+                    <Icon 
+                        onClick={() => {}} 
+                        type="pencil-square" 
+                        color="warning" 
+                        classes={["me-3"]} 
+                    />
+                    <Icon 
+                        onClick={() => {}} 
+                        type="trash" 
+                        color="danger" 
+                    />
+                </div>
+            </li>
+        ))}
     </ul>
-  )
+)
 }
--- client/src/global/slices/todosSlice.js
+++ client/src/global/slices/todosSlice.js
@@ -3,15 +3,25 @@ import { createSlice } from "@reduxjs/toolkit"
 export const { reducer, actions } = createSlice ({
   name: "todos",
   initialState: {
-    isLoading: false,
-    todos: [],
+    todos: [
+      {
+        id : 1,
+        text: "This is my first todo",
+        isComplete : true
+      },
+      {
+        id : 2,
+        text: "This is my second todo",
+        isComplete : false
+      }
+    ],
+      isLoading : false
   },
   reducers: {
-    clearTodos () {
-
+    clearTodos (state) {
+      state.todos = []
+      state.numDispatches = 0
     }
   },
-  extraReducers () {
-
-  }
+  extraReducers ({addCase}) {}
 })
