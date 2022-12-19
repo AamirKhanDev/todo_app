538ffd700843ef3f4e5f7c1906c58788fb862dbe
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-19 21:40:08 +0000

    Add in ToDoText component for our List

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,12 +1,25 @@
+import Icon from "./Icon"
+import TodoText from "./ToDoText"
+
 export default function List () {
   return (
     <ul id="list" className="list-group rounded-0 pb-3">
       <li className="list-group-item d-flex justify-content-between align-items-center">
-        This is the text to show
+        <TodoText/>
         <div>
-          <Icon/>
+          <Icon
+            onClick={() =>{}}
+            type="pencil-square"
+            color="warning"
+            class={("me-3")}
+          />
+          <Icon
+            onClick={() =>{}}
+            type="trash"
+            color="danger"
+          />
         </div>
       </li>
     </ul>
-)
+  )
 }
