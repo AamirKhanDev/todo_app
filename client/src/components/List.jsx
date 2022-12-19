e971b149985e4ecc37e603585113889817e3c2a2
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-19 21:29:22 +0000

    Add in Icon component for our List component. Started working on List component

---
+++ client/src/components/List.jsx
@@ -0,0 +1,12 @@
+export default function List () {
+  return (
+    <ul id="list" className="list-group rounded-0 pb-3">
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        This is the text to show
+        <div>
+          <Icon/>
+        </div>
+      </li>
+    </ul>
+)
+}
